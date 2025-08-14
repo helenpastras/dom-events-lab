fa81424f2a9b3b6b6434036d53aa893a3eadfff5
helenpastras <helenapastras@hotmail.com>
2025-08-14 17:31:25 -0400

    a little more code

--- app.js
+++ app.js
@@ -9,44 +9,56 @@
 // clear operation to start from begining (0?)
 //
 /*-------------------------------- Constants --------------------------------*/
+
+/*-------------------------------- Variables --------------------------------*/
+let resultTotal;
+let num1 = '';
+let num2 = '';
+let operation = null;
+
+/*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
 const display = document.querySelector('.display');
 
-
-/*-------------------------------- Variables --------------------------------*/
-let resultTotal;
-let currentInput = '';
-let previousInput = '';
-let operation = null;
-
-/*------------------------ Cached Element References ------------------------*/
-
 /*----------------------------- Event Listeners -----------------------------*/
 buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
-    // This log is for testing purposes to verify we're getting the correct value
-    console.log("this logs the button clicked" , event.target.innerText);
-    // Future logic to capture the button's value would go here...
-  });
+   const buttonText = button.textContent;
+
+      if (event.target.classList.contains('number')) {
+        num1 += buttonText;
+        display.value = num1
+    } else if (event.target.classList.contains('operator')){
+          if (num1 !== '') {
+            operator = buttonText;
+          display.value = operator;
+          }
+    } else if (event.target.classList.contains('clear')){
+          num1 = '';
+          operator = null;
+          display.value = ''; 
+    }
+  })
 });
 
 
-calculator.addEventListener('click', (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  console.log(event.target.innerText);
+//   if (event.target.innerText === '*') {
+//     // Do something with this operator
+//     console.log("this logs the button clicked" , event.target.innerText);
+//     // Future logic to capture the button's value would go here...
+//   });
+// });
+
+
+// calculator.addEventListener('click', (event) => {
+//   // This log is for testing purposes to verify we're getting the correct value
+//   // You have to click a button to see this log
+//   console.log(event.target.innerText);
 
   // Example
-  if (event.target.classList.contains('number')) {
-    // appendNumber(what are we appending?)
-  }
 
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
-  }
-});
+// });
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -54,6 +66,9 @@ const init = () => {
 console.log('it loaded');
 } 
 
+const calculate = () =>{
+
+}
 
 
 function updateDisplay() {
@@ -61,14 +76,13 @@ function updateDisplay() {
 }
 
 function appendNumber(number) {
-    if (number = true)// Prevent multiple decimals
-    currentInput += number;
+    if (number = true)
+    num1 += number;
     updateDisplay();
 }
 
 const render = () => {
     // resultTotal.textContent = `${result}`;
-    updateDisplay();
     appendNumber();
     console.log("it rendered");
 };
